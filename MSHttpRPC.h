261c7a384de86a7b3cffee8dd3ccb3bc6a4c182e
stonedong <yishuiliunian@gmail.com>
2015-03-26 14:21:03 +0800

    添加全局头文件

---
+++ MSHttpRPC.h
@@ -0,0 +1,22 @@
+//
+//  MSHttpRPC.h
+//  Pods
+//
+//  Created by stonedong on 15/3/26.
+//
+//
+
+#ifndef Pods_MSHttpRPC_h
+#define Pods_MSHttpRPC_h
+
+#import "MSHttpLog.h"
+#import "MSRequest.h"
+#import "MSRequestOperation.h"
+#import "MSRouter.h"
+#import "MSSyncCenter.h"
+#import "MSToken.h"
+#import "MSTokenManager.h"
+#import "MSTokenRequest.h"
+#import "MSVerifyAuthorRequest.h"
+
+#endif
